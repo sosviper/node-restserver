bef8265e378177a91e67e04511eab61c6dae8cfa
Victor Valencia <sosviperdigital@gmail.com>
2020-01-04 23:52:52 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://127.0.0.1:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://sosviper:NGJhcr1eyQ52tNXv@cluster0-2bsxx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
